97804baddf0f1da4abaa06c3aa36252a2266a7cc
jorainer <johannes.rainer@gmail.com>
2020-01-31 13:52:09 +0100

    Add the shell script to call the extract timestamp

---
+++ extract_timestamp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# R=/shared/bioinf/R/R-3.6.0-BioC3.9/bin/R
+# scrpt="~/Projects/git/EuracBiomedicalResearch/batch_centroid/centroiding.R"
+#  $R --vanilla --file=$scrpt
+
+R=R
+scrpt="/Users/jo/Projects/git/EuracBiomedicalResearch/batch_centroid/extract_timestamp.R"
+$R --file=$scrpt
