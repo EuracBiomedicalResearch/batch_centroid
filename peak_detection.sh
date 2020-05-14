3e30a244a8e5b6c5c4a02693269b1e6a9ba22089
jorainer <johannes.rainer@gmail.com>
2020-05-14 15:07:40 +0200

    Update peak detection script to use R-4.0.0

--- peak_detection.sh
+++ peak_detection.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/shared/bioinf/R/R-3.6.1-BioC3.10/bin/R --file="/home/mgarciaaloy/git/EuracBiomedicalResearch/batch_centroid/peak_detection.R"
+/shared/bioinf/R/R-4.0.0/bin/R --file="/home/jrainer/Projects/git/EuracBiomedicalResearch/batch_centroid/peak_detection.R"
 
