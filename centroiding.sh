930b78233217a13da8b7fb0de091b018214a9c59
Johannes Rainer <jrainer@gm.eurac.edu>
2020-09-18 08:52:34 +0200

    Use the singularity-R for centroiding

--- centroiding.sh
+++ centroiding.sh
@@ -3,6 +3,6 @@
 # scrpt="~/Projects/git/EuracBiomedicalResearch/batch_centroid/centroiding.R"
 #  $R --vanilla --filscrpt
 
-R=/shared/bioinf/R/R-4.0.0-BioC3.11/bin/R
+R=/shared/bioinf/R/bin/R-4.0.0-BioC3.11
 scrpt="/home/jrainer/Projects/git/EuracBiomedicalResearch/batch_centroid/centroiding.R"
 $R --file=$scrpt
